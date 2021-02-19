6db9e96f69726e0af77a831e730b3ddf604c9f2c
Strigon009 <42129076+Strigon009@users.noreply.github.com>
2021-02-19 12:16:58 +0800

    Wave counter, multiplier

---
+++ Assets/Scripts/Gameplay/WaveSystem/WaveDisplay.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class WaveDisplay : MonoBehaviour
+{
+    // Various Texts
+    public TextMeshProUGUI WaveCountText;
+    public TextMeshProUGUI EnemiesLeftText;
+    public TextMeshProUGUI TimeTillSpawnText;
+
+    public GameObject WaveSystem;
+
+    private int WaveCount = 0;
+    private int EnemiesLeftCount = 0;
+    private float TimeCount = 30f;
+
+    private float WaitTimer = 5f;
+    private bool TriggerThis = true;
+
+    void Update()
+    {
+        // Timer till next wave
+        if (TimeCount > 0)
+            TimeCount -= Time.deltaTime;
+        // Spawn next wave
+        else
+        {
+            WaitTimer = 0f;
+            TimeCount = 30f;
+            TriggerThis = true;
+            WaveSystem.SetActive(true);
+        }
+
+        if (WaitTimer < 2f)
+            WaitTimer += Time.deltaTime;
+        else
+        {
+            if (TriggerThis == true)
+            {
+                if (EnemiesLeftCount == 0)
+                {
+                    if (TimeCount > 5f)
+                    {
+                        TimeCount = 5f;
+                    }
+                    TriggerThis = false;
+                }
+            }
+        }
+        // What wave it is
+        WaveCount = WaveSystem.GetComponent<WaveSystem>().waveCount;
+        WaveCountText.SetText("Wave: " + WaveCount);
+
+        // How many enemies left
+        EnemiesLeftCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
+        EnemiesLeftText.SetText("Enemies Left: " + EnemiesLeftCount.ToString());
+
+        // Time Display
+        TimeTillSpawnText.SetText("Time Left: " + (int)TimeCount);
+    }
+
+    void ReduceTimer()
+    {
+    }
+}
--- Assets/Scripts/Gameplay/WaveSystem/WaveSystem.cs
+++ Assets/Scripts/Gameplay/WaveSystem/WaveSystem.cs
@@ -11,48 +11,70 @@ public class WaveSystem : MonoBehaviour
 
     [SerializeField] private List<GameObject> enemies;
     // public GameObject enemy;
-    public int xPos;
-    public int zPos;
-    public int enemyCount;
+    public static int xPos;
+    public static int zPos;
+    public static int enemyCount;
 
     // Where the enemy will want to spawn
     private int oldSpawn;
     private int chosenSpawn = 0;
 
+    public int waveCount = 0;
+
     // Max Enemy Count. Will increase after a few rounds
-    private int enemyCountMax = 10;
+    private int enemyCountMax = 0;
     // Rate of enemies spawning in. Will decrease after a few rounds
     private float spawnInterval = 1f;
 
     private void OnEnable()
     {
-        StartCoroutine(SpawnEnemy());
-    }
-    private void OnDisable()
-    {
+        waveCount += 1;
+
         if (spawnInterval > 0.1f)
             spawnInterval -= 0.05f;
 
-        if (enemyCountMax > 50)
-            enemyCountMax = 50;
+        if (enemyCountMax > 70)
+            enemyCountMax = 70;
         else
-            enemyCountMax += (enemyCountMax / 5);
+            enemyCountMax = GetNthFibonacci_Ite(waveCount + 1);
 
         Debug.Log("Max Enemies: " + enemyCountMax);
         Debug.Log("SpawnInterval: " + spawnInterval);
 
         enemyCount = 0;
+
+        StartCoroutine(SpawnEnemy());
+
+        Debug.Log("Enemies Remaining: " + GameObject.FindGameObjectsWithTag("Enemy").Length);
+    }
+
+    private void OnDisable()
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (enemyCount == enemyCountMax)
+            gameObject.SetActive(false);
+    }
+
+    // Fibonacci Sequence to spawn enemies
+    public static int GetNthFibonacci_Ite(int n)
+    {
+        int number = n - 1; //Need to decrement by 1 since we are starting from 0  
+        int[] Fib = new int[number + 1];
+        Fib[0] = 0;
+        Fib[1] = 1;
+        for (int i = 2; i <= number; i++)
+        {
+            Fib[i] = Fib[i - 2] + Fib[i - 1];
+        }
+        return Fib[number];
     }
 
     IEnumerator SpawnEnemy()
     {
-        Debug.Log(enemyCountMax);
-        
         // Spawn enemies while count less than max
         while (enemyCount < enemyCountMax)
         {
